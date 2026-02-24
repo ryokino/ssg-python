e2bdf184193a65dd6395c5c71d9ee1fc35034b18
ryokino <ryo.kinoshita212@gmail.com>
2026-02-25 01:27:19 +0900

    feat: 新しいParentNodeクラスとそのto_htmlメソッドを追加し、関連するテストを実装しました。

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,3 +33,21 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag must be provided for ParentNode")
+        if self.children is None:
+            raise ValueError("Children must be provided for ParentNode")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.children}, {self.props})"
